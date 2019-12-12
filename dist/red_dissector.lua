3048206e7098681b998eb33c10390720a3c1a758
zhangyi-13572252156 <46628913+zhangyi-13572252156@users.noreply.github.com>
2019-12-12 20:07:39 +0800

    Add files via upload

---
+++ dist/red_dissector.lua
@@ -0,0 +1,91 @@
+local version_str = string.match(_VERSION, "%d+[.]%d*")
+local version_num = version_str and tonumber(version_str) or 5.1
+local bit = (version_num >= 5.2) and require("bit32") or require("bit")
+
+-- create a new dissector to decode rtp private payload
+
+local NAME1          = "red"
+local PORT           = 5004
+local RTP_PROTO_TYPE = 106
+
+
+local red            = Proto(NAME1, "Red Protocol")
+
+
+-- create fields of red
+fields_M             = ProtoField.uint8 (NAME1 .. ".M", "M", base.HEX,Payload_type,0x80)
+fields_pt            = ProtoField.uint8 (NAME1 .. ".PT", "PT", base.DEC,Payload_type,0x7F)
+fields_seqno         = ProtoField.uint16(NAME1 .. ".seqno", "Sequence number")
+fields_h264bytes     = ProtoField.bytes(NAME1 .. ".bytes", "H264Data")
+fields_fec           = ProtoField.bytes(NAME1 .. ".fec", "FEC Payload")
+
+red.fields           = { fields_M, fields_pt, fields_seqno, fields_h264bytes,fields_fec }
+
+
+local RTP_dis        = Dissector.get("rtp")
+local H264_dis       = Dissector.get("h264")
+local Data_dis       = Dissector.get("data")
+
+
+-- dissect packet
+function red.dissector(tvb, pinfo, tree)
+	length = tvb:len()
+	if length == 0 then return end
+	
+    -- decode private header
+    local subtree = tree:add(red, tvb(0,3))
+	
+	subtree:add(fields_M, tvb(0,1))
+	
+	subtree:add(fields_pt, tvb(0,1))
+	
+    subtree:add(fields_seqno, tvb(1,2))
+
+    -- show protocol name in protocol column
+    pinfo.cols.protocol = red.name
+   	
+	local fec_id = tvb(0,1):uint()
+	local fec_type = bit.band(fec_id,0x7F)
+	if fec_type == 109 then 
+		tree:add(fields_fec,tvb(3))
+	else 
+		H264_dis:call(tvb(3):tvb(), pinfo, tree)
+	end 
+	
+	--[[
+	local proto_id = tvb(3,1):uint()
+	local proto_type = bit.band(proto_id,0x1F)
+	if proto_type >= 1 and proto_type <= 23 then -- one nual
+		H264_dis:call(tvb(3,1):tvb(),pinfo,tree)
+		tree:add(fileds_h264bytes,tvb(3))	
+	elseif proto_type >= 24 and proto_type <= 27 then  -- STAP-A sps/pps
+		H264_dis:call(tvb(3,1):tvb(),pinfo,tree)
+		tree:add(fileds_h264bytes,tvb(3))
+	elseif proto_type == 28 then -- FU-A
+		H264_dis:call(tvb(3,2):tvb(),pinfo,tree)
+		tree:add(fields_h264bytes,tvb(3))
+	else
+		H264_dis:call(tvb(3):tvb(), pinfo, tree)
+	end
+	]]--
+    -- decode h264 data
+	--local len = (tvb:len()-3)-1
+    --H264_dis:call(tvb(3):tvb(), pinfo, tree)
+	--H264_dis:call(tvb(3,2):tvb(), pinfo, tree)
+    --tree:add(fields_h264bytes,tvb(3))
+end
+
+
+--decode first layer  as rtp
+local udp_dissector_table = DissectorTable.get("udp.port")
+udp_dissector_table:set(PORT,RTP_dis)
+
+-- register this dissector
+-- DissectorTable.get("rtp.pt"):add(PORT, red)
+--decode private protocol layer  3-bytes private datas + standard h264
+local rtp_dissector_table = DissectorTable.get("rtp.pt")
+rtp_dissector_table:set(RTP_PROTO_TYPE,red)
+
+
+
+
